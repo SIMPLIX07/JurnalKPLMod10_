5b547a31ede309bbc4583d2b9163ba47e2534c87
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-30 16:40:34 +0700

    menambahkan api

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class MovieController : Controller
+    {
+        private static List<string> actor1 = new List<string>
+        {
+            "Chris Hems",
+            "Emma Stone",
+            "Emma Myers"
+        };
+        private static List<string> actor2 = new List<string>
+        {
+            "Tom hiddlestone",
+            "Benedict Cumberbatch",
+            "beifong"
+        };
+        private static List<string> actor3 = new List<string>
+        {
+            "Tom Hanks",
+            "Robert Downy",
+            "Chris Evans"
+        };
+        private static List<Movie> _movieList = new List<Movie>
+        {
+            new Movie("Avenger","Nolan", "Super hero", actor1),
+            new Movie("Avatar Aang", "Twinkle toes", "Negara api membuat kekacauan", actor2),
+            new Movie("Spiderman","Nolan hemsworth", "Super hero laba", actor3)
+        };
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return _movieList;
+        }
+
+        [HttpGet("{index}")]
+        public Movie GetByIndex(int index)
+        {
+            if (index < 0 || index > _movieList.Count)
+            {
+                return null;
+            }
+            return _movieList[index];
+        }
+
+        [HttpPost]
+        public void AddMovie([FromBody] Movie movie)
+        {
+            _movieList.Add(movie);
+
+        }
+
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+
+            _movieList.RemoveAt(index);
+
+        }
+    }
+}
---
+++ Movie.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Description { get; set; }
+        public List <string> Stars { get; set; }
+
+        public Movie(string title, string director, string description, List<string> stars)
+        {
+            Title = title;
+            Director = director;
+            Description = description;
+            Stars = stars;
+
+        }
+    }
+}
